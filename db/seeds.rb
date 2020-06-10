c7d854630fe8a9106d4b95bfa5de7bf9bd65b7de
Yasushi Arai <araiyasushi550@gmail.com>
2020-06-10 04:30:26 +0000

    タスク一覧ページ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 
-User.create!( name: "Sample User",
+User.create!( name: "管理人",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+
+Task.create!( title: "タスク",
+              note: "タスク詳細",
+              user_id: "1")
