3cf56cca5e645845dbc3618e59f35d42097038ae
Yasushi Arai <araiyasushi550@gmail.com>
2020-06-10 15:41:18 +0000

    タスクの新規作成と編集更新ページ作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,23 +2,41 @@ class TasksController < ApplicationController
   
   before_action :set_user
   
-  def index
-    @tasks = @user.tasks
-  end
-  
-  def create
-  end
-  
   def new
   end
   
-  def edit
+  def create
+    @task = Task.new(title: params[:title],
+                     note: params[:note],
+                     user_id: params[:user_id])
+    if @task.save
+      flash[:success] = "タスクを作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
+  
+  def index
+    @tasks = @user.tasks
   end
   
   def show
   end
   
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
   def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(title: params[:title],
+                               note: params[:note])
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :edit
+    end
   end
   
   def destroy
