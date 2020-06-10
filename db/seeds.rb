ba12ea249076106ed944ff13df929916efaa0255
Yasushi Arai <araiyasushi550@gmail.com>
2020-06-10 08:08:44 +0000

    サンプル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,21 @@ User.create!( name: "管理人",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+              
+99.times do |n|
+  name = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password
+    )
+end
 
-Task.create!( title: "タスク",
-              note: "タスク詳細",
-              user_id: 1)
+@users = User.order(:created_at).take(3)
+50.times do |n|
+  title = Faker::Lorem.sentence(2)
+  note = Faker::Lorem.sentence(5)
+  @users.each { |user| user.tasks.create!(title: title, note: note)}
+end
